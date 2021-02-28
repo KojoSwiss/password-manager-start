a9325fef6ebea48bcbd5b7814782c7f85a4a6300
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-28 14:28:59 +0100

    validation of fields

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
-import pandas
+from tkinter import messagebox
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -8,10 +10,16 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("pandora.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please fill all the fields")
+    else:
+        is_ok = messagebox.askyesno(title=website, message=f"Please confirm: \nEmail: {email}\nPassword: {password}\n Is it ok?")
+
+        if is_ok:
+            with open("pandora.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -37,7 +45,6 @@ email_label.grid(column=0, row=2)
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, "kojo.atieku@gmail.com")
-
 
 password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
