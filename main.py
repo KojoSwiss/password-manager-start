1be9afa64bc7128bbf9d2c9b5512be7e881313d1
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-28 13:37:30 +0100

    UI created

--- main.py
+++ main.py
@@ -13,6 +13,30 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 canvas_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=canvas_image)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website: ")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username: ")
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+password_label = Label(text="Password")
+password_label.grid(column=0, row=3)
+
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+
+gen_btn = Button(text="Generate Password")
+gen_btn.grid(column=2, row=3)
+
+add_btn = Button(width=36, text="Add")
+add_btn.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
