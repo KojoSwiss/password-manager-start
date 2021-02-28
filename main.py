0335f943848a812b6fbe81166e4417215500bc1f
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-28 14:20:09 +0100

    Create an external list

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from tkinter import *
+import pandas
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
 
+    with open("pandora.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -20,12 +29,15 @@ website_label.grid(column=0, row=1)
 
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text="Email/Username: ")
 email_label.grid(column=0, row=2)
 
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "kojo.atieku@gmail.com")
+
 
 password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
@@ -36,7 +48,7 @@ password_entry.grid(column=1, row=3)
 gen_btn = Button(text="Generate Password")
 gen_btn.grid(column=2, row=3)
 
-add_btn = Button(width=36, text="Add")
+add_btn = Button(width=36, text="Add", command=save)
 add_btn.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
